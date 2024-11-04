3f8c81ca1d63f1d6e5bfd675c31ea9c743add55c
tz8 <tz8@nyu.edu>
2024-11-04 12:46:55 +0800

    update font

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,15 +5,24 @@
  */
 
 // Styles
+import 'material-design-icons-iconfont/dist/material-design-icons.css' 
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, md } from 'vuetify/iconsets/md'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'light',
+  },
+  icons: {
+    defaultSet: 'md', // This is already the default value - only for display purposes
+    aliases,
+    sets: {
+      md,
+    },
   },
 })
